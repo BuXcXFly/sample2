eca93704ff8c46353a8b289626e45091c9278ee1
XcX <buxcxfly@gmail.com>
2017-07-22 05:35:27 +0000

    edit1

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,6 +13,13 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'only' => ['edit', 'update']
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -42,4 +49,31 @@ class UsersController extends Controller
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update($id, Request $request)
+     {
+         $this->validate($request, [
+             'name' => 'required|max:50',
+             'password' => 'confirmed|min:6'
+         ]);
+
+         $user = User::findOrFail($id);
+
+         $data = [];
+         $data['name'] = $request->name;
+         if ($request->password) {
+             $data['password'] = bcrypt($request->password);
+         }
+         $user->update($data);
+
+         session()->flash('success', '个人资料更新成功！');
+
+         return redirect()->route('users.show', $id);
+     }
 }
